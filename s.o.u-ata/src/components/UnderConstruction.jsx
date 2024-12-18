328a969b9692149720673dfc3bced77277f1dc58
Arian Xhumkar <arianxhumkar@hotmail.com>
2024-12-19 00:41:09 +0100

    Added an Under Construction component while building the website

---
+++ s.o.u-ata/src/components/UnderConstruction.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UnderConstruction = () => {
+    return (
+        <div style={{ textAlign: "center", marginTop: "20%" }}>
+            <h1>We're Under Construction!</h1>
+            <p>Our website is coming soon. Stay tuned!</p>
+        </div>
+    );
+};
+
+export default UnderConstruction;
