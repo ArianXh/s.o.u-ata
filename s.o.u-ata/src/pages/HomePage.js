8ad17114a959025d7cfc663bc8e48a91172bcbc1
Arian Xhumkar <arianxhumkar@hotmail.com>
2024-12-18 15:43:44 +0100

    Removed default files of React, setup the demo Homepage with the routes, added TailwindCSS to the project. Project ready to get worked on

---
+++ s.o.u-ata/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1 class="text-red-600 font-bold">
+            Hello worldddd!
+        </h1>
+    </div>
+  )
+}
+
+export default HomePage
